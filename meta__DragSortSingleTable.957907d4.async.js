5e47849f7804df3e1f20f8e301e31fe910a950c1
庞聪刚 <pangconggang@pangconggangdeMacBook-Pro.local>
2025-05-28 02:21:00 +0800

    Updates

---
+++ meta__DragSortSingleTable.957907d4.async.js
@@ -0,0 +1,48 @@
+"use strict";(self.webpackChunkpangcg_components=self.webpackChunkpangcg_components||[]).push([[51],{68974:function(d,a,e){var t;e.r(a),e.d(a,{demos:function(){return g}});var l=e(17061),r=e.n(l),s=e(17156),p=e.n(s),o=e(67294),u=e(34122),I=e(28063),g={"dragsortsingletable-demo-demo":{component:o.memo(o.lazy(function(){return e.e(803).then(e.bind(e,56616))})),asset:{type:"BLOCK",id:"dragsortsingletable-demo-demo",refAtomIds:["DragSortSingleTable"],dependencies:{"index.tsx":{type:"FILE",value:e(23302).Z},antd:{type:"NPM",value:"5.25.3"},"pangcg-components":{type:"NPM",value:"0.0.5"},react:{type:"NPM",value:"18.3.1"}},entry:"index.tsx"},context:{antd:u,"pangcg-components":I,react:t||(t=e.t(o,2))},renderOpts:{compile:function(){var m=p()(r()().mark(function v(){var c,x=arguments;return r()().wrap(function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,e.e(335).then(e.bind(e,37335));case 2:return n.abrupt("return",(c=n.sent).default.apply(c,x));case 3:case"end":return n.stop()}},v)}));function i(){return m.apply(this,arguments)}return i}()}}}},26950:function(d,a,e){e.r(a),e.d(a,{texts:function(){return t}});const t=[{value:"DragSortSingleTable \u662F\u4E00\u4E2A\u57FA\u4E8E @dnd-kit/core \u5B9E\u73B0\u7684\u53EF\u62D6\u62FD\u6392\u5E8F\u7684\u8868\u683C\u7EC4\u4EF6\uFF0C\u7EE7\u627F\u81EA Ant Design \u7684 Table \u7EC4\u4EF6\u3002",paraId:0,tocIndex:0},{value:"\u5F53\u4F60\u9700\u8981\u4EE5\u8868\u683C\u5C55\u793A\u6570\u636E\uFF0C\u5E76\u4E14\u5E0C\u671B\u7528\u6237\u80FD\u591F\u901A\u8FC7\u62D6\u62FD\u6765\u8C03\u6574\u6570\u636E\u987A\u5E8F\u65F6",paraId:1,tocIndex:1},{value:"\u9002\u7528\u4E8E\u9700\u8981\u81EA\u5B9A\u4E49\u5217\u8868\u987A\u5E8F\u4E14\u9700\u8981\u4FDD\u5B58\u6392\u5E8F\u7ED3\u679C\u7684\u573A\u666F",paraId:1,tocIndex:1},{value:"\u5F53\u4F60\u9700\u8981\u4E00\u4E2A\u7B80\u5355\u6613\u7528\u7684\u62D6\u62FD\u6392\u5E8F\u8868\u683C\uFF0C\u53C8\u4E0D\u60F3\u81EA\u5DF1\u5B9E\u73B0\u590D\u6742\u7684\u62D6\u62FD\u903B\u8F91\u65F6",paraId:1,tocIndex:1},{value:"\u57FA\u4E8E Ant Design \u7684 Table \u7EC4\u4EF6\uFF0C\u62E5\u6709\u5176\u6240\u6709\u529F\u80FD",paraId:2,tocIndex:2},{value:"\u652F\u6301\u5782\u76F4\u65B9\u5411\u7684\u884C\u62D6\u62FD\u6392\u5E8F",paraId:2,tocIndex:2},{value:"\u53EF\u4EE5\u81EA\u5B9A\u4E49\u62D6\u62FD\u628A\u624B\u7684\u6837\u5F0F",paraId:2,tocIndex:2},{value:"\u63D0\u4F9B\u6392\u5E8F\u7ED3\u675F\u540E\u7684\u56DE\u8C03\u51FD\u6570\uFF0C\u65B9\u4FBF\u5904\u7406\u6392\u5E8F\u540E\u7684\u6570\u636E",paraId:2,tocIndex:2},{value:"rowKey \u7ED1\u5B9A\u7684\u5B57\u6BB5\u6570\u636E\u6E90\u4E00\u5B9A\u662F\u5B58\u5728\u4E14\u552F\u4E00\u7684\uFF0C \u9ED8\u8BA4\u662F id \u5B57\u6BB5",paraId:3,tocIndex:3},{value:"\u53C2\u6570",paraId:4,tocIndex:5},{value:"\u8BF4\u660E",paraId:4,tocIndex:5},{value:"\u7C7B\u578B",paraId:4,tocIndex:5},{value:"\u9ED8\u8BA4\u503C",paraId:4,tocIndex:5},{value:"\u7248\u672C",paraId:4,tocIndex:5},{value:"initialDataSource",paraId:4,tocIndex:5},{value:"\u8868\u683C\u6570\u636E\u6E90",paraId:4,tocIndex:5},{value:"RecordType[]",paraId:4,tocIndex:5},{value:"[]",paraId:4,tocIndex:5},{value:"0.0.5",paraId:4,tocIndex:5},{value:"isDrag",paraId:4,tocIndex:5},{value:"\u662F\u5426\u542F\u7528\u62D6\u62FD\u529F\u80FD",paraId:4,tocIndex:5},{value:"boolean",paraId:4,tocIndex:5},{value:"true",paraId:4,tocIndex:5},{value:"0.0.5",paraId:4,tocIndex:5},{value:"columns",paraId:4,tocIndex:5},{value:"\u8868\u683C\u5217\u7684\u914D\u7F6E\u63CF\u8FF0",paraId:4,tocIndex:5},{value:"TableColumnType<RecordType>[]",paraId:4,tocIndex:5},{value:"-",paraId:4,tocIndex:5},{value:"0.0.5",paraId:4,tocIndex:5},{value:"renderDragHandle",paraId:4,tocIndex:5},{value:"\u81EA\u5B9A\u4E49\u62D6\u62FD\u628A\u624B",paraId:4,tocIndex:5},{value:"React.ReactNode",paraId:4,tocIndex:5},{value:"-",paraId:4,tocIndex:5},{value:"0.0.5",paraId:4,tocIndex:5},{value:"onDragSortEnd",paraId:4,tocIndex:5},{value:"\u62D6\u62FD\u6392\u5E8F\u7ED3\u675F\u540E\u7684\u56DE\u8C03\u51FD\u6570",paraId:4,tocIndex:5},{value:"(newDataSource: RecordType[]) => void",paraId:4,tocIndex:5},{value:"-",paraId:4,tocIndex:5},{value:"0.0.5",paraId:4,tocIndex:5},{value:"\u9664\u4E86\u4EE5\u4E0A\u5C5E\u6027\u5916\uFF0C\u8BE5\u7EC4\u4EF6\u8FD8\u652F\u6301 Ant Design Table \u7EC4\u4EF6\u7684\u6240\u6709\u5C5E\u6027\u3002",paraId:5,tocIndex:5}]},23302:function(d,a){a.Z=`import { message, TableColumnsType } from 'antd';
+import { DragSortSingleTable, generateUUID } from 'pangcg-components';
+import React, { useState } from 'react';
+
+interface DataType {
+  id: string;
+  name: string;
+  age: number;
+  address: string;
+}
+
+const DemoPage = () => {
+  const columns: TableColumnsType<DataType> = [
+    { title: 'Name', dataIndex: 'name' },
+    { title: 'Age', dataIndex: 'age' },
+    { title: 'Address', dataIndex: 'address' },
+  ];
+
+  const initialData: DataType[] = Array.from({ length: 15 }, (_, index) => ({
+    id: generateUUID(),
+    name: \`\u7528\u6237 \${index + 1}\`,
+    age: Math.floor(Math.random() * 50) + 18,
+    address: \`\u57CE\u5E02 \${String.fromCharCode(
+      65 + Math.floor(Math.random() * 26),
+    )} \u7B2C \${index + 1} \u8857\u533A\`,
+  }));
+
+  const [dataSource, setDataSource] = useState<DataType[]>([...initialData]);
+
+  return (
+    <div>
+      <DragSortSingleTable<DataType>
+        rowKey="id"
+        initialDataSource={dataSource}
+        columns={columns}
+        pagination={false}
+        onDragSortEnd={(newList) => {
+          message.success('\u6570\u636E\u66F4\u65B0\u6210\u529F');
+          setDataSource([...newList]);
+          console.log('-----', newList);
+        }}
+      />
+    </div>
+  );
+};
+
+export default DemoPage;
+`}}]);
---
+++ meta__DragSortTreeTable.38d4ba57.async.js
@@ -0,0 +1,90 @@
+"use strict";(self.webpackChunkpangcg_components=self.webpackChunkpangcg_components||[]).push([[735],{91003:function(d,n,e){var t;e.r(n),e.d(n,{demos:function(){return g}});var l=e(17061),r=e.n(l),p=e(17156),I=e.n(p),o=e(67294),s=e(34122),u=e(28063),g={"dragsorttreetable-demo-demo":{component:o.memo(o.lazy(function(){return e.e(105).then(e.bind(e,51901))})),asset:{type:"BLOCK",id:"dragsorttreetable-demo-demo",refAtomIds:["DragSortTreeTable"],dependencies:{"index.tsx":{type:"FILE",value:e(12428).Z},antd:{type:"NPM",value:"5.25.3"},"pangcg-components":{type:"NPM",value:"0.0.5"},react:{type:"NPM",value:"18.3.1"}},entry:"index.tsx"},context:{antd:s,"pangcg-components":u,react:t||(t=e.t(o,2))},renderOpts:{compile:function(){var i=I()(r()().mark(function v(){var c,x=arguments;return r()().wrap(function(a){for(;;)switch(a.prev=a.next){case 0:return a.next=2,e.e(335).then(e.bind(e,37335));case 2:return a.abrupt("return",(c=a.sent).default.apply(c,x));case 3:case"end":return a.stop()}},v)}));function m(){return i.apply(this,arguments)}return m}()}}}},37982:function(d,n,e){e.r(n),e.d(n,{texts:function(){return t}});const t=[{value:"DragSortTreeTable \u662F\u4E00\u4E2A\u57FA\u4E8E @dnd-kit/core \u5B9E\u73B0\u7684\u53EF\u62D6\u62FD\u6392\u5E8F\u7684\u6811\u5F62\u8868\u683C\u7EC4\u4EF6\uFF0C\u7EE7\u627F\u81EA Ant Design \u7684 Table \u7EC4\u4EF6\u3002",paraId:0,tocIndex:0},{value:"\u5F53\u4F60\u9700\u8981\u4EE5\u8868\u683C\u5C55\u793A\u6570\u636E\uFF0C\u5E76\u4E14\u5E0C\u671B\u7528\u6237\u80FD\u591F\u901A\u8FC7\u62D6\u62FD\u6765\u8C03\u6574\u6570\u636E\u987A\u5E8F\u65F6",paraId:1,tocIndex:1},{value:"\u9002\u7528\u4E8E\u9700\u8981\u81EA\u5B9A\u4E49\u5217\u8868\u987A\u5E8F\u4E14\u9700\u8981\u4FDD\u5B58\u6392\u5E8F\u7ED3\u679C\u7684\u573A\u666F",paraId:1,tocIndex:1},{value:"\u5F53\u4F60\u9700\u8981\u4E00\u4E2A\u7B80\u5355\u6613\u7528\u7684\u62D6\u62FD\u6392\u5E8F\u8868\u683C\uFF0C\u53C8\u4E0D\u60F3\u81EA\u5DF1\u5B9E\u73B0\u590D\u6742\u7684\u62D6\u62FD\u903B\u8F91\u65F6",paraId:1,tocIndex:1},{value:"\u57FA\u4E8E Ant Design \u7684 Table \u7EC4\u4EF6\uFF0C\u62E5\u6709\u5176\u6240\u6709\u529F\u80FD",paraId:2,tocIndex:2},{value:"\u652F\u6301\u5782\u76F4\u65B9\u5411\u7684\u884C\u62D6\u62FD\u6392\u5E8F",paraId:2,tocIndex:2},{value:"\u53EF\u4EE5\u81EA\u5B9A\u4E49\u62D6\u62FD\u628A\u624B\u7684\u6837\u5F0F",paraId:2,tocIndex:2},{value:"\u63D0\u4F9B\u6392\u5E8F\u7ED3\u675F\u540E\u7684\u56DE\u8C03\u51FD\u6570\uFF0C\u65B9\u4FBF\u5904\u7406\u6392\u5E8F\u540E\u7684\u6570\u636E",paraId:2,tocIndex:2},{value:"rowKey \u7ED1\u5B9A\u7684\u5B57\u6BB5\u6570\u636E\u6E90\u4E00\u5B9A\u662F\u5B58\u5728\u4E14\u552F\u4E00\u7684\uFF0C \u9ED8\u8BA4\u662F id \u5B57\u6BB5",paraId:3,tocIndex:3},{value:"\u53C2\u6570",paraId:4,tocIndex:5},{value:"\u8BF4\u660E",paraId:4,tocIndex:5},{value:"\u7C7B\u578B",paraId:4,tocIndex:5},{value:"\u9ED8\u8BA4\u503C",paraId:4,tocIndex:5},{value:"\u7248\u672C",paraId:4,tocIndex:5},{value:"initialDataSource",paraId:4,tocIndex:5},{value:"\u8868\u683C\u6570\u636E\u6E90",paraId:4,tocIndex:5},{value:"RecordType[]",paraId:4,tocIndex:5},{value:"[]",paraId:4,tocIndex:5},{value:"0.0.5",paraId:4,tocIndex:5},{value:"isDrag",paraId:4,tocIndex:5},{value:"\u662F\u5426\u542F\u7528\u62D6\u62FD\u529F\u80FD",paraId:4,tocIndex:5},{value:"boolean",paraId:4,tocIndex:5},{value:"true",paraId:4,tocIndex:5},{value:"0.0.5",paraId:4,tocIndex:5},{value:"columns",paraId:4,tocIndex:5},{value:"\u8868\u683C\u5217\u7684\u914D\u7F6E\u63CF\u8FF0",paraId:4,tocIndex:5},{value:"TableColumnType<RecordType>[]",paraId:4,tocIndex:5},{value:"-",paraId:4,tocIndex:5},{value:"0.0.5",paraId:4,tocIndex:5},{value:"renderDragHandle",paraId:4,tocIndex:5},{value:"\u81EA\u5B9A\u4E49\u62D6\u62FD\u628A\u624B",paraId:4,tocIndex:5},{value:"React.ReactNode",paraId:4,tocIndex:5},{value:"-",paraId:4,tocIndex:5},{value:"0.0.5",paraId:4,tocIndex:5},{value:"onDragSortEnd",paraId:4,tocIndex:5},{value:"\u62D6\u62FD\u6392\u5E8F\u7ED3\u675F\u540E\u7684\u56DE\u8C03\u51FD\u6570",paraId:4,tocIndex:5},{value:"(newDataSource: RecordType[]) => void",paraId:4,tocIndex:5},{value:"-",paraId:4,tocIndex:5},{value:"0.0.5",paraId:4,tocIndex:5},{value:"childrenColumnName",paraId:4,tocIndex:5},{value:"\u6307\u5B9A\u6811\u5F62\u7ED3\u6784\u7684\u5B50\u8282\u70B9\u5B57\u6BB5\u540D",paraId:4,tocIndex:5},{value:"string",paraId:4,tocIndex:5},{value:"children",paraId:4,tocIndex:5},{value:"0.0.5",paraId:4,tocIndex:5},{value:"parentIdKey",paraId:4,tocIndex:5},{value:"\u6307\u5B9A\u7236\u8282\u70B9 ID \u7684\u5B57\u6BB5\u540D",paraId:4,tocIndex:5},{value:"string",paraId:4,tocIndex:5},{value:"parentId",paraId:4,tocIndex:5},{value:"0.0.5",paraId:4,tocIndex:5},{value:"rowKey",paraId:4,tocIndex:5},{value:"\u8868\u683C\u884C key \u7684\u53D6\u503C",paraId:4,tocIndex:5},{value:"string | ((record: RecordType) => string)",paraId:4,tocIndex:5},{value:"id",paraId:4,tocIndex:5},{value:"0.0.5",paraId:4,tocIndex:5},{value:"\u9664\u4E86\u4EE5\u4E0A\u5C5E\u6027\u5916\uFF0C\u8BE5\u7EC4\u4EF6\u8FD8\u652F\u6301 Ant Design Table \u7EC4\u4EF6\u7684\u6240\u6709\u5C5E\u6027\u3002",paraId:5,tocIndex:5}]},12428:function(d,n){n.Z=`import { message, TableColumnsType } from 'antd';
+import { DragSortTreeTable, generateUUID } from 'pangcg-components';
+import React, { useState } from 'react';
+
+interface DataType {
+  id: string;
+  name: string;
+  age: number;
+  address: string;
+  children?: DataType[];
+  parentId?: string;
+}
+
+const DemoPage = () => {
+  const columns: TableColumnsType<DataType> = [
+    { title: '\u540D\u79F0', dataIndex: 'name' },
+    { title: '\u5E74\u9F84', dataIndex: 'age' },
+    { title: '\u5730\u5740', dataIndex: 'address' },
+  ];
+
+  // \u751F\u6210\u6811\u5F62\u6570\u636E
+  const generateTreeData = (
+    depth = 0,
+    parentId?: string,
+    parentIndex?: string | number,
+  ): DataType[] => {
+    if (depth >= 3) return []; // \u6700\u591A\u751F\u62103\u5C42
+
+    const count = depth === 0 ? 5 : Math.floor(Math.random() * 3) + 1; // \u7B2C\u4E00\u5C425\u4E2A\uFF0C\u5B50\u8282\u70B91-3\u4E2A\u968F\u673A
+
+    return Array.from({ length: count }, (_, index) => {
+      const id = generateUUID();
+      const nodeIndex =
+        parentIndex !== undefined
+          ? \`\${parentIndex}-\${index + 1}\`
+          : \`\${index + 1}\`;
+      const nodeData: DataType = {
+        id,
+        parentId,
+        name: \`\u8282\u70B9 \${nodeIndex}\`,
+        age: Math.floor(Math.random() * 50) + 18,
+        address: \`\u57CE\u5E02 \${String.fromCharCode(
+          65 + Math.floor(Math.random() * 26),
+        )} \u7B2C \${nodeIndex} \u8857\u533A\`,
+      };
+
+      const children = generateTreeData(depth + 1, id, nodeIndex);
+      if (children.length > 0) {
+        nodeData.children = children;
+      }
+
+      return nodeData;
+    });
+  };
+
+  const initialData: DataType[] = generateTreeData();
+  const [dataSource, setDataSource] = useState<DataType[]>([...initialData]);
+
+  // \u6DFB\u52A0\u9009\u4E2D\u884C\u7684\u72B6\u6001\u7BA1\u7406
+  // const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
+
+  // \u591A\u9009\u529F\u80FD\u76F8\u5173\u914D\u7F6E
+  // const rowSelection = {
+  //   selectedRowKeys,
+  //   onChange: (newSelectedRowKeys: React.Key[]) => {
+  //     console.log('-newSelectedRowKeys--', newSelectedRowKeys);
+  //     setSelectedRowKeys(newSelectedRowKeys);
+  //   },
+  // };
+
+  return (
+    <div>
+      <DragSortTreeTable<DataType>
+        rowKey="id"
+        initialDataSource={dataSource}
+        columns={columns}
+        pagination={false}
+        onDragSortEnd={(newList) => {
+          message.success('\u6570\u636E\u66F4\u65B0\u6210\u529F');
+          setDataSource([...newList]);
+          console.log('\u6811\u5F62\u6570\u636E\u66F4\u65B0:', newList);
+        }}
+        // rowSelection={rowSelection}
+      />
+    </div>
+  );
+};
+
+export default DemoPage;
+`}}]);
